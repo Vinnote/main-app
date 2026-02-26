309a9fb004595706df70d97fd4d4e3741352b44e
Henri costa rios <henricostarios@gmail.com>
2026-02-26 12:48:31 -0300

    adapted some screens to gluestack

--- components/ui/gluestack-ui-provider/index.tsx
+++ components/ui/gluestack-ui-provider/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { config } from './config';
 import { View, ViewProps } from 'react-native';
 import { OverlayProvider } from '@gluestack-ui/core/overlay/creator';
@@ -15,13 +15,17 @@ export function GluestackUIProvider({
   children?: React.ReactNode;
   style?: ViewProps['style'];
 }) {
-  const { colorScheme } = useColorScheme();
-  const activeColorScheme = mode === 'system' ? colorScheme : mode;
+  const { colorScheme, setColorScheme } = useColorScheme();
+
+  useEffect(() => {
+    setColorScheme(mode);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mode]);
 
   return (
     <View
       style={[
-        config[activeColorScheme ?? 'light'],
+        config[colorScheme!],
         { flex: 1, height: '100%', width: '100%' },
         props.style,
       ]}
